3e850d51e7816f5772eee59c921906b65fd800b5
Никита Ангелов <angeloff555@mail.ru>
2023-03-17 19:38:06 +0300

    update test_capitalize.py

--- tests/test_capitalize.py
+++ tests/test_capitalize.py
@@ -1,7 +1,9 @@
 from capitalize import capitalize
 
-assert capitalize('hello') == 'Hello'
+if capitalize('hello') != 'Hello':
+    raise Exception('Функция работает неверно!')
 
-assert capitalize('') == ''
+if capitalize('') != '':
+    raise Exception('Функция работает неверно!')
 
 print('Все тесты пройдены!')
